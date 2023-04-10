f1bee82ff614b014effa7dbc9cc132673425421a
PoojaNarawad <poojanarawad16@gmail.com>
2023-04-10 16:11:21 +0530

    initial commit

---
+++ countdownapp.js
@@ -0,0 +1,22 @@
+const countdownElement = document.getElementById('countdown');
+
+const countDownDate = new Date("Feb 5, 2025 23:59:59").getTime();
+
+const countdownInterval = setInterval(function() {
+
+  const now = new Date().getTime();
+
+  const distance = countDownDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  countdownElement.innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+
+  if (distance < 0) {
+    clearInterval(countdownInterval);
+    countdownElement.innerHTML = "EXPIRED";
+  }
+}, 1000);
